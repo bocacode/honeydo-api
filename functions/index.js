a5e02865f86d4e6ff7d8265929af7010ef6d6dd3
Tda <tdorluss@gmail.com>
2021-10-27 21:16:16 -0400

    added update and delete endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,15 @@
 const functions = require("firebase-functions")
 const express = require('express')
 const cors = require('cors')
-const { createUserItem, getUserItems } = require('./src/items')
+const { createUserItem, getUserItems, updateItem, deactiveItem } = require('./src/items')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.post('/items', createUserItem)
-app.get('/items/:uid', getUserItems)
+app.get('/users/:uid/items', getUserItems)
+app.patch('/items/:itemId', updateItem)
+app.delete('/users/:uid/items/:itemId', deactiveItem)
 
 exports.app = functions.https.onRequest(app)
--- functions/src/items.js
+++ functions/src/items.js
@@ -1,10 +1,12 @@
 const { getClient } = require('./dbConnection')
+const ObjectId = require('mongodb').ObjectId
 
 exports.getUserItems = async (req, res) => {
   const { uid } = req.params
   const client = await getClient()
   const db = client.db('Honey-Do')
   const collection = db.collection('items')
+  
   try {
     const results = collection.find({ 'uid': uid })
     const items = await results.toArray()
@@ -38,4 +40,67 @@ exports.createUserItem = async (req, res) => {
   } catch(error) {
     res.status(500).send(error)
   }
+}
+
+exports.updateItem = async (req, res) => {
+  const { name, uid, done } = req.body
+  const itemId = req.params.itemId
+
+  if(!uid){
+    res.status(401).send('Invalid Request')
+  }
+
+  const updateDoc = { updated_at: new Date()}
+  
+  if (name) {
+    updateDoc.name = name
+  }
+
+  if (done) {
+    updateDoc.done = done 
+  }
+  try {
+
+    const itemCollection = await getItemCollection()
+    const result = await itemCollection.updateOne(
+    {'_id': new ObjectId(itemId), 'uid': uid }, 
+    { $set: updateDoc})
+    res.status(200).send(result)
+    
+  } catch (error) {
+      console.log(error)
+      res.status(500).send(error)
+  }
+  
+
+}
+
+exports.deactiveItem = async (req, res) => {
+  console.log(req.params)
+  const { itemId, uid } = req.params
+
+  if(!uid || !itemId){
+    res.status(500).send('Invalid Request')
+  }
+
+  try {
+    const collection = await getItemCollection()
+    //const result = await collection.updateOne({'_id': new ObjectId(itemId), 'uid': uid})
+    const result = await collection.updateOne(
+      {'_id': new ObjectId(itemId), 'uid': uid }, 
+      { $set: {'active': false, 'updated_at': new Date()}})
+
+      res.status(200).send(result)
+
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+
+async function getItemCollection() {
+  const client = await getClient()
+  const db = client.db('Honey-Do')
+  const collection = db.collection('items')
+
+  return collection
 }
